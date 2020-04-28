af60ba0c79ec30e78b7b3220ba1d83eb33fd159e
Alexander P. Valencia <alex16jpv@gmail.com>
2020-04-28 13:31:48 -0500

    add useFetch

--- src/container/App.jsx
+++ src/container/App.jsx
@@ -1,31 +1,21 @@
 import React, { useEffect, useState } from 'react';
 import Store from '../components/Store';
 import StoreItem from '../components/StoreItem';
+import useFetch from '../hooks/useFetch';
 import '../assets/styles/App.scss';
 
 
+const lang = navigator.language.slice(0,2);
+let url = `https://fortniteapi.io/shop?lang=${lang}`;
+const config = {
+    method: 'GET',
+    headers: {
+        Authorization: 'a79e8da4-60310edd-84b45098-cc3e1bbe',
+    }
+};
+
 export default () => {
-    const [ items, setItems ] = useState({
-        daily: [], featured: [], specialDaily: [], specialFeatured: []
-    });
-
-    useEffect(()=>{
-        const lang = navigator.language.slice(0,2);
-        let url = `https://fortniteapi.io/shop?lang=${lang}`;
-        const config = {
-            method: 'GET',
-            headers: {
-                Authorization: 'a79e8da4-60310edd-84b45098-cc3e1bbe',
-            }
-        };
-
-        fetch(url, config)
-            .then(response => response.json())
-            .then(data => {
-                setItems(data);              
-            })
-    }, []);
-
+    const items = useFetch(url, config);
     return(
         <div className="app">
             <h1 className='store__titles'>Fortnite store today!!</h1>
